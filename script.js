8b4dabe7d412893f87edd7ed2495789a80e8dadf
Akram Mohamed <akram.mohamed0@hotmail.com>
2021-04-05 17:58:17 +0200

    about section added

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 window.onload = ()=> {
     console.log(window.innerHeight);
 }
+
+let navbar = document.getElementById('navbar');
+window.onscroll = () => {
+    if(window.pageYOffset>0){
+        navbar.classList.add('bg-sec');
+    } else {
+        navbar.classList.remove('bg-sec');
+    }
+}
