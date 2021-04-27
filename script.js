5fa49e1a61a914693a0d2d14ed40d8b89997e435
Akram Mohamed <akram.mohamed0@hotmail.com>
2021-04-27 17:45:18 +0200

    responsive layout done successfully

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 window.onload = () => {
     let splides = document.getElementsByClassName('splide');
-    new Splide(splides[0], {
-        rewind: true,
-        perPage: 3,
-        perMove: 1,
-        autoplay: true,
-      }).mount();
-      new Splide(splides[1], {
-        rewind: true,
-        perPage: 3,
-        perMove: 1,
-        autoplay: true,
-      }).mount();
+    for(splide of splides) {
+        new Splide(splide, {
+            rewind: true,
+            perPage: 3,
+            perMove: 1,
+            autoplay: true,
+            breakpoints: {
+                575: {
+                    perPage: 2,
+                },
+                400: {
+                    perPage: 1,
+                }
+            }
+          }).mount();
+    }
 }
 
 email = () => {
 window.location.href = "mailto:akram.mohamed.dev@gmail.com";
 }
 
+window.addEventListener('load', windowHandler);
+window.addEventListener('resize', windowHandler);
+
+function windowHandler() {
+    var header = document.getElementById('header');
+    var headerText = header.getElementsByClassName('headerText')[0];
+    var headerImg = header.getElementsByClassName('headerImg')[0];
+    if(window.innerWidth<1300){
+        headerText.classList.remove('col-lg-6');
+        headerText.classList.add('col-lg-7');
+        headerImg.classList.remove('offset-xl-2');
+        headerImg.classList.add('offset-xl-1');
+    } else {
+        headerText.classList.add('col-lg-6');
+        headerText.classList.remove('col-lg-7');
+        headerImg.classList.add('offset-xl-2');
+        headerImg.classList.remove('offset-xl-1');
+    }
+}
 
 
 let navbar = document.getElementById('navbar');
@@ -49,18 +72,23 @@ function openImg(src) {
     node.style.width = `300px`;
 }
 
-modals.forEach(function(trigger) {
-  trigger.addEventListener('click', function(event) {
-    event.preventDefault();
-    var modal = document.getElementById(trigger.dataset.modal);
-    modal.classList.add('open');
-    var modalContainer = modal.querySelector('.modal-container');
-    modalContainer.appendChild(node);
-    var exit = modal.querySelector('.modal-exit');
-      exit.addEventListener('click', function(event) {
+if(window.innerWidth>1200){
+    modals.forEach(function(trigger) {
+        trigger.addEventListener('click', function(event) {
         event.preventDefault();
-        modal.classList.remove('open');
-      });
-  });
-});
+        var modal = document.getElementById(trigger.dataset.modal);
+        modal.classList.add('open');
+        var modalContainer = modal.querySelector('.modal-container');
+        modalContainer.appendChild(node);
+        var exit = modal.querySelector('.modal-exit');
+            exit.addEventListener('click', function(event) {
+            event.preventDefault();
+            modal.classList.remove('open');
+            });
+        });
+    });
+}
+
+
+
 
